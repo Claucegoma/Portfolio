6fe81a1db5dfc739914b881eccff0211938ed924
claucegoma <arzagitana@ciencias.unam.mx>
2023-12-12 19:12:46 -0600

    working on testing

---
+++ jest.config.cjs
@@ -0,0 +1,9 @@
+
+// jest.config.cjs
+module.exports = {
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['js'],
+  testMatch: ['<rootDir>/test/**/*.test.js'],
+ 
+  
+};
--- test/popup-window.test.js
+++ test/popup-window.test.js
@@ -9,9 +9,9 @@ describe('PopupWindow component', () => {
   });
 
   it('displays the correct title', async () => {
-    const el = await fixture(html`<popup-window title="Test"></popup-window>`);
+    const el = await fixture(html`<popup-window title="Burger queen Api client"></popup-window>`);
     const title = el.shadowRoot.querySelector('h2');
-    expect(title.textContent).to.include('Test');
+    expect(title.textContent).to.include('Burger queen Api client');
   });
 
   it('closes the window on button click', async () => {
@@ -32,3 +32,59 @@ describe('PopupWindow component', () => {
 
   // Añade más pruebas según sea necesario
 });
+
+/* 
+import { html, fixture, expect } from '@open-wc/testing';
+import './path/to/PopupWindow'; // Reemplaza con la ruta real
+
+describe('PopupWindow', () => {
+  it('se renderiza con valores predeterminados', async () => {
+    const el = await fixture(html`<popup-window></popup-window>`);
+    expect(el.title).to.equal('');
+    expect(el.content).to.deep.equal({});
+    // Agrega más expectativas según los valores predeterminados
+  });
+
+  it('se renderiza con el contenido proporcionado', async () => {
+    const el = await fixture(html`
+      <popup-window .title=${'Título de prueba'} .content=${{
+        image: './ruta/a/imagen-de-prueba.png',
+        link1: 'https://ejemplo.com',
+        label1: 'Enlace de ejemplo',
+        link2: 'https://ejemplo2.com',
+        label2: 'Enlace de ejemplo 2',
+        icons: ['./ruta/a/icono1.png', './ruta/a/icono2.png'],
+      }}></popup-window>
+    `);
+
+    expect(el.title).to.equal('Título de prueba');
+    expect(el.content).to.deep.equal({
+      image: './ruta/a/imagen-de-prueba.png',
+      link1: 'https://ejemplo.com',
+      label1: 'Enlace de ejemplo',
+      link2: 'https://ejemplo2.com',
+      label2: 'Enlace de ejemplo 2',
+      icons: ['./ruta/a/icono1.png', './ruta/a/icono2.png'],
+    });
+    // Agrega más expectativas según el contenido proporcionado
+  });
+
+  it('llama al método cerrarVentana cuando se hace clic en el botón', async () => {
+    const el = await fixture(html`<popup-window></popup-window>`);
+    const spy = jest.spyOn(el, 'cerrarVentana');
+    el.shadowRoot.querySelector('.button').click();
+    expect(spy).toHaveBeenCalledTimes(1);
+    spy.mockRestore();
+  });
+
+  it('cierra la ventana al hacer clic fuera del componente', async () => {
+    const el = await fixture(html`<popup-window></popup-window>`);
+    const spy = jest.spyOn(el, 'remove');
+    
+    // Simula un clic fuera del componente
+    document.body.click();
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    spy.mockRestore();
+  });
+}); */
